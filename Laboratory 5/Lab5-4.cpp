5663366a478deb9ec52b6e57cb57f2ec28043dfa
GnatishinMikhail <71734946+GnatishinMikhail@users.noreply.github.com>
2020-09-28 22:44:56 +0300

    Update Lab5-4.cpp
    Changed code of programm to proper task

--- Laboratory 5/Lab5-4.cpp
+++ Laboratory 5/Lab5-4.cpp
@@ -1,23 +1,21 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
 		setlocale(LC_ALL, "Rus");
-	
-	float x1, y1, x2, y2, x3, y3;
-	cout << "Введите координаты трех вершин треугольника: ";
-	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
 
-	float a, b, c, p, P, S;
-	a = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-	b = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
-	c = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
-	P = a + b + c;
-	p = P / 2; //полупериметр
-	S = sqrt(p * (p - a) * (p - b) * (p - c));
+	float x1, y1, x2, y2;
+	cout << "Введите координаты двух противоположных вершин прямоугольника: ";
+	cin >> x1 >> y1 >> x2 >> y2;
 
-	cout << "Периметр данного треугольника равен " << P << endl;
-	cout << "Площадь данного треугольника равна " << S << endl;
+	float a, b, P, S;
+	a = abs(x2 - x1);
+	b = abs(y2 - y1);
+	P = 2 * (a + b);
+	S = a * b;
+
+	cout << "Периметр данного прямоугольника равен " << P << endl;
+	cout << "Площадь данного прямоугольника равна " << S << endl;
 
 	return 0;
 }
