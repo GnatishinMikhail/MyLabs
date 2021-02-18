65cbe3ec25f1400e454133f227b7e1e4006c1696
GnatishinMikhail <71734946+GnatishinMikhail@users.noreply.github.com>
2021-02-18 21:32:58 +0300

    Add files via upload

---
+++ Lab1-1 VS.cpp
@@ -0,0 +1,27 @@
+// generates the poem
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    cout << "Не выходи из комнаты не совершай ошибку              \n"
+         "Зачем тебе Солнце, если ты куришь Шипку?                \n"
+         "За дверью бессмысленно всё, особенно — возглас счастья. \n"
+         "Только в уборную — и сразу же возвращайся.              \n";
+
+    return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
